fd0ee2600a9cf2b8c9135b58f664cf530d1eb317
inghamn <inghamn@c0873d58-3416-0410-9558-ca141da00fa8>
2006-05-11 19:46:12 +0000

    Repopulated the scripts directory for this repository.  Usernames and passwords showed up in various revisions and the files had to be removed.  They will be added back in once they are cleaned.
    git-svn-id: svn+ssh://bandit.bloomington.in.gov/srv/svn/master_address/trunk@27 c0873d58-3416-0410-9558-ca141da00fa8

---
+++ scripts/1_import.sql
@@ -0,0 +1,356 @@
+----------------------------------------------------------------------------
+-- DATA Cleanup
+----------------------------------------------------------------------------
+-- We need to make sure that all the street_numbers are valid before importing the data
+-- Make sure that there's spaces after each number and before any letter or fraction
+-- Also make sure that we don't have any numbers that aren't numbers.
+select street_address_id,street_number,street_id from mast_address where street_number regexp '[a-zA-Z]';
+
+-- Once we're done with the import, we're going to come back to this list
+-- The places on this list should really be subunits.  But it'll be
+-- easier to fix once we've got the data into the new schema.
+select street_address_id,street_number,street_id from mast_address where street_number is null;
+
+----------------------------------------------------------------------------
+-- Target checkist
+-- Data for these tables needs to imported in this order, to preserve
+-- the correct foreign key relationships
+----------------------------------------------------------------------------
+
+-- routes
+-- routeStreets
+-- Actually, routes do not exist in the old data.  There's nothing to import yet
+
+-- roles
+insert roles (role) select distinct contact_type from oldAddressData.mast_addr_assignment_contact;
+
+-- users
+insert users (id,firstname,lastname,phone,department)
+select contact_id,first_name,last_name,phone_number,agency from oldAddressData.mast_addr_assignment_contact;
+
+-- userRoles
+insert user_roles select contact_id,id from oldAddressData.mast_addr_assignment_contact
+left join roles on contact_type=role;
+
+-- stateRoads
+insert stateRoads select * from oldAddressData.state_road_master;
+
+-- towns
+insert towns select * from oldAddressData.towns_master;
+
+-- townships
+insert townships select * from oldAddressData.township_master;
+
+-- jurisdictions
+insert jurisdictions select * from oldAddressData.governmental_jurisdiction_mast;
+
+-- cities
+insert cities (name) select distinct city from oldAddressData.mast_address;
+
+-- statusCodes
+--		This is actually combining 3 tables of status codes into one.  The original
+--		status_code integers won't be valid anymore.  When importing status fields
+--		you will need to join those and compare the description with this statusCode.status
+--		for the correct statusCode to use
+-- We also need an UNKNOWN status for the ones that are blank
+insert statuses values(1,"UNKNOWN");
+
+insert statuses (status) (select description from oldAddressData.buildings_status_lookup)
+union (select description from oldAddressData.mast_address_status_lookup)
+union (select description from oldAddressData.mast_street_status_lookup)
+order by description;
+
+-- suffixes
+insert suffixes (suffix,description) select * from oldAddressData.mast_street_type_suffix_master;
+
+-- streetNameTypes
+insert streetNameTypes (type,description) select * from oldAddressData.mast_street_name_type_master;
+
+-- districtTypes
+insert districtTypes (type) select distinct type from oldAddressData.addr_location_purpose_mast order by type;
+
+-- placeTypes
+insert placeTypes (type,description) select * from oldAddressData.addr_location_types_master;
+
+-- unitTypes
+insert unitTypes (type,description) select * from oldAddressData.mast_addr_subunit_types_mast;
+
+-- buildingTypes
+insert buildingTypes select * from oldAddressData.building_types_master;
+
+-- trashPickupDays
+insert trashPickupDays (day) select * from oldAddressData.trash_pickup_master;
+
+-- recyclingPickupWeeks
+insert recyclingPickupWeeks (week) select * from oldAddressData.trash_recycle_week_master;
+
+-- directions
+insert directions (code,direction) select * from oldAddressData.mast_street_direction_master;
+
+-- annexations
+insert annexations (ordinanceNumber,name) select ordinance_number,name from oldAddressData.annexations;
+
+-- districts
+insert districts select location_purpose_id,description,districtTypes.id
+from oldAddressData.addr_location_purpose_mast left join districtTypes using (type);
+
+-- platType
+-- This data doesn't exist anywhere yet.
+insert platTypes (type,description) values('S','Residential Subdivision');
+insert platTypes (type,description) values('C','Commercial');
+insert platTypes (type,description) values('A','Amended');
+
+-- plats
+insert plats select plat_id,name,township_id,platTypes.id,plat_cabinet,envelope,notes
+from oldAddressData.plat_master left join platTypes on plat_type=type;
+
+
+
+-- names
+insert names (town_id,direction_id,name,suffix_id,postDirection_id,startDate,endDate,notes)
+select distinct town_id,d.id,street_name,suffixes.id,p.id,effective_start_date,effective_end_date,mast_street_names.notes
+from oldAddressData.mast_street_names left join oldAddressData.mast_street using (street_id)
+left join directions d on mast_street_names.street_direction_code=d.code
+left join suffixes on street_type_suffix_code=suffixes.suffix
+left join directions p on mast_street_names.post_direction_suffix_code=p.code;
+
+
+
+-- segments
+-- Segments have COUNTY in as a location, which should equate to Monroe County
+update oldAddressData.segments set location='MONROE COUNTY' where location='COUNTY';
+
+-- Clean up the travel_d information
+update oldAddressData.segments set travel_d=null where travel_d not in ('N','S','E','W');
+
+-- The segments have a BUILT status that hasn't been added to statusCodes yet
+insert statuses set status='BUILT';
+
+-- We need some usernames in the new data, so we can assign correct userIDs for the last updated fields
+update users set username='haleyl' where firstname='LAURA' and lastname='HALEY';
+update users set username='goodmanr' where firstname='RUSS' and lastname='GOODMAN';
+insert users set username='winklec',authenticationMethod='LDAP',firstname='Chuck',lastname='Winkle',department='ITS',phone='812-349-3595';
+
+insert segments select null,tag,lowadd,highadd,j.id,s.id,comments,speed,indotID,class,maintain,
+leftlow,lefthigh,rightlow,righthigh,rcinode1,rcinode2,low_node,high_node,
+low_x,low_y,high_x,high_y,travel,d.id,complete,rclback,rclahead,class_row,
+maparea,lastdate,up_act,u.id
+from oldAddressData.segments left join jurisdictions j on location=j.name
+left join directions d on travel_d=d.code
+left join users u on up_by=username
+left join statuses s on seg_stat=status;
+
+alter table segments add index (tag);
+
+-- Some final cleanup on segments
+update segments set transportationClass=null where transportationClass="\'";
+update segments set transportationClass=null where transportationClass="-";
+update segments set transportationClass=null where transportationClass="0";
+
+
+
+-- streets
+insert streets select distinct streetID,notes,ifnull(s.id,1) from oldAddressData.segments
+left join oldAddressData.mast_street on streetID=street_id
+left join oldAddressData.mast_street_status_lookup using (status_code)
+left join statuses s on description=s.status
+where streetID>0 order by streetID;
+
+-- streetSegments
+insert street_segments select streetID,s.id
+from oldAddressData.segments left join segments s using (tag)
+where streetID>0;
+
+-- streetNames
+-- You will need to run PHP -> importStreetNames.php at this point
+-- I could not figure out a way to get it to work using just SQL.  It wasn't macthing the fields
+-- in the new names table.
+
+
+
+
+-- places
+-- Run cleanLocations.php now  this will combine the locations that should really be the same place
+
+-- We need to create some temp tables, that we can clean out as we import the data we want.
+create table tempLocations like oldAddressData.address_location;
+insert tempLocations select * from oldAddressData.address_location;
+delete from tempLocations where subunit_id is not null;
+
+
+insert places
+select distinct l.location_id,common_name,township_id,gov_jur_id,t.id,g.id,r.id,mailable_flag,livable_flag,section,quarter_section,location_class
+from tempLocations l left join oldAddressData.mast_address a using (street_address_id)
+left join oldAddressData.mast_address_sanitation s using (street_address_id)
+left join oldAddressData.locations_classes c on l.location_id=c.location_id
+left join trashPickupDays t on trash_pickup_day=t.day
+left join trashPickupDays g on large_item_pickup_day=g.day
+left join recyclingPickupWeeks r on recycle_week=r.week
+group by location_id;
+
+
+-- placeHistory
+-- There are some contacts that just aren't in the system anymore.  We should clear those contact_id's out
+update oldAddressData.mast_address_assignment_hist set contact_id=null where contact_id not in (select id from users);
+
+insert placeHistory select location_id,action,action_date,notes,contact_id from oldAddressData.mast_address_assignment_hist
+where location_id in (select id from places);
+
+-- units
+-- Run PHP-> importUnits at this point.
+-- We'll need to clean out the data that we imported, though
+-- This should zero out tempLocations
+delete from tempLocations where location_id in (select id from places);
+
+-- Confirm that it's zero before dropping
+select count(*) from tempLocations;
+drop table tempLocations;
+
+
+-- placeStatus
+insert place_status
+select p.id,effective_start_date,effective_end_date,s.id
+from places p left join oldAddressData.mast_address_location_status on p.id=location_id
+left join oldAddressData.mast_address_status_lookup using (status_code)
+left join statuses s on description=status;
+
+
+-- addresses
+-- Now we can import all the addresses
+-- run 1-4-importAddresses.php
+-- Check the noSegmentData.txt file that this generates.  These will all be problem addresses
+
+
+-- districtPlaces
+insert district_places
+select location_id,location_purpose_id from oldAddressData.addr_location_purposes
+where location_id in (select id from places);
+
+
+-- buildings
+-- I think these are going to have to be on hold for now.  The data's just to wonky.
+-- Right now, we can just link the units directly to the locations.  Come back later
+-- and do the buildings
+
+-- This would import the buildings, if we didn't have buildings that somehow existed on multiple locations
+-- insert buildings
+-- select t.building_id,l.placeID,building_name,effective_start_date,effective_end_date,gis_tag,statusCode
+-- from places l left join oldAddressData.building_address_location on placeID=location_id
+-- left join tempBuildings t using(building_id)
+-- left join oldAddressData.buildings_status_lookup using (status_code)
+-- left join statusCodes on description=status;
+
+
+-- The rest of these tables don't have complete data in the old system.
+-- I'm putting their import on hold for now.
+-- subdivisions
+-- subdivisionNames
+-- segmentPlats
+
+-------------------------------------------------------------------------------
+-- Source checklist
+-- Make a note on these tables when you have all the data accounted for in
+-- the import section above
+-------------------------------------------------------------------------------
+
+
+-- IMPORTED	addr_location_purposes
+-- IMPORTED	annexations
+-- IMPORTED	segments
+-- IMPORTED	address_location
+-- IMPORTED	addr_location_purpose_mast
+-- IMPORTED	addr_location_types_master
+-- IMPORTED	buildings_status_lookup
+-- IMPORTED	building_types_master
+-- IMPORTED	governmental_jurisdiction_mast
+-- IMPORTED	locations_classes
+-- IMPORTED	mast_address
+-- IMPORTED	mast_addr_assignment_contact
+-- IMPORTED	mast_address_assignment_hist
+-- IMPORTED	mast_address_location_status
+-- IMPORTED	mast_addr_subunit_types_mast
+-- IMPORTED	mast_address_location_change
+-- IMPORTED	mast_address_sanitation
+-- IMPORTED	mast_address_status
+-- IMPORTED	mast_address_status_lookup
+-- IMPORTED	mast_street
+-- IMPORTED	mast_street_direction_master
+-- IMPORTED	mast_street_names
+-- IMPORTED	mast_street_name_type_master
+-- IMPORTED mast_street_status_lookup
+-- IMPORTED	mast_street_type_suffix_master
+-- IMPORTED	plat_master
+-- IMPORTED	state_road_master
+-- IMPORTED	towns_master
+-- IMPORTED	township_master
+-- IMPORTED	trash_pickup_master
+-- IMPORTED	trash_recycle_week_master
+-- IMPORTED	mast_address_subunits
+
+-- ON HOLD	building_address_location		Data needs to be cleaned up big time before we can
+-- ON HOLD	buildings						bring these two tables in
+
+-- ON HOLD	subdivision_master				We're missing a lot of data to be able to bring subdivions
+-- ON HOLD	subdivision_names				in.  We need plats to come in first, for example.
+
+
+-- IGNORED	mast_address_subunit_status		This table should really be the same data as the location status
+
+-- IGNORED	mast_addr_assgn_hist_temp		Old temp table, data is not relevant
+-- IGNORED	mast_address_parcel				Table has no data
+-- IGNORED	addr_location_rel_types_mast	Table has no data
+-- IGNORED	addr_location_relationships		Table has no data
+-- IGNORED	addr_jurisdiction_master 		This table is fully contained in governamtal_jurisdiction_mast
+-- IGNORED	mast_address_annexation			Table has no data
+-- IGNORED	mast_street_addr_jurisdictions	Table has no data
+-- IGNORED	mast_street_gov_jurisdictions	Table has no data
+-- IGNORED	mast_street_plat				Table has no data
+-- IGNORED	mast_street_section				Table has no data
+-- IGNORED	mast_street_state_road			Table has no data
+-- IGNORED	mast_street_subdivision			Table has no data
+-- IGNORED	mast_street_townships			Table has no data
+-- IGNORED	parcel							Table has no data
+-- IGNORED	trash_large_item_pickup_master  This table is equivilent to trash_pickup_master
+-- IGNORED	quarter_section_master			Just a lookup table with four directions
+
+
+----------------------------------------------------------------------------
+-- DATA Cleanup
+----------------------------------------------------------------------------
+select street_address_id,street_number,street_id from mast_address where street_number regexp '[a-zA-Z]';
+
+
+----------------------------------------------------------------------------
+-- Fulltext search index creation
+----------------------------------------------------------------------------
+create table addressIndex (
+fullAddress varchar(255) not null,
+address_id int unsigned not null,
+segment_id int unsigned not null,
+street_id int unsigned not null,
+name_id int unsigned not null,
+place_id int unsigned not null,
+unit_id int unsigned,
+fulltext (fullAddress),
+foreign key (address_id) references addresses (id),
+foreign key (segment_id) references segments (id),
+foreign key (street_id) references streets (id),
+foreign key (name_id) references names (id),
+foreign key (place_id) references places (id),
+foreign key (unit_id) references units (id)
+) engine=MyISAM;
+
+
+insert into addressIndex
+select concat_ws(' ',addresses.number,d.code,names.name,x.suffix,p.code,unitTypes.type,units.identifier),
+addresses.id as address_id,segments.id as segment_id,streets.id as street_id,names.id as name_id,addresses.place_id,units.id as unit_id
+from addresses left join segments on addresses.segment_id=segments.id
+left join street_segments on segments.id=street_segments.segment_id
+left join streets on street_segments.street_id=streets.id
+left join street_names on streets.id=street_names.street_id
+left join names on street_names.name_id=names.id
+left join units on addresses.place_id=units.place_id
+left join unitTypes on unitType_id=unitTypes.id
+left join directions d on names.direction_id=d.id
+left join directions p on names.postDirection_id=p.id
+left join suffixes x on names.suffix_id=x.id;
---
+++ scripts/master_address.sql
@@ -0,0 +1,344 @@
+CREATE TABLE towns (
+	id int unsigned auto_increment NOT NULL,
+	name varchar(30) UNIQUE NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE routes (
+	id int unsigned auto_increment NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE subdivisions (
+	id int unsigned auto_increment NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE townships (
+	id int unsigned auto_increment NOT NULL,
+	name varchar(128) UNIQUE NOT NULL,
+	abbreviation char(2) UNIQUE NOT NULL,
+	quarterCode char(1),
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE jurisdictions (
+	id int unsigned auto_increment NOT NULL,
+	name varchar(128) UNIQUE NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE directions (
+	id int unsigned auto_increment NOT NULL,
+	code char(2) UNIQUE NOT NULL,
+	direction varchar(30) UNIQUE NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE statuses (
+	id int unsigned auto_increment NOT NULL,
+	status varchar(30) UNIQUE NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE suffixes (
+	id int unsigned auto_increment NOT NULL,
+	suffix char(4) UNIQUE NOT NULL,
+	description varchar(30) UNIQUE NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE streetNameTypes (
+	id int unsigned auto_increment NOT NULL,
+	type varchar(30) UNIQUE NOT NULL,
+	description varchar(128) UNIQUE NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE districtTypes (
+	id int unsigned auto_increment NOT NULL,
+	type varchar(128) UNIQUE NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE trashPickupDays (
+	id int unsigned auto_increment NOT NULL,
+	day enum('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday') DEFAULT 'Monday' UNIQUE NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE recyclingPickupWeeks (
+	id int unsigned auto_increment NOT NULL,
+	week char(1) UNIQUE NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE annexations (
+	id int unsigned auto_increment NOT NULL,
+	ordinanceNumber varchar(12) NOT NULL,
+	name varchar(128),
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE platTypes (
+	id int unsigned auto_increment NOT NULL,
+	type char(1) UNIQUE NOT NULL,
+	description varchar(128) UNIQUE NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE placeTypes (
+	id int unsigned auto_increment NOT NULL,
+	type varchar(40) NOT NULL,
+	description varchar(255) NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE unitTypes (
+	id int unsigned auto_increment NOT NULL,
+	type varchar(10) NOT NULL,
+	description varchar(128) NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE buildingTypes (
+	id int unsigned NOT NULL,
+	description varchar(128) NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE stateRoads (
+	id int unsigned  NOT NULL,
+	name varchar(128) NOT NULL,
+	abbreviation varchar(15) NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE cities (
+	id int unsigned auto_increment NOT NULL,
+	name varchar(30) UNIQUE,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE users (
+	id int unsigned auto_increment NOT NULL,
+	username varchar(30) UNIQUE,
+	password varchar(32),
+	authenticationMethod varchar(40) DEFAULT 'LDAP' NOT NULL,
+	firstname varchar(128) NOT NULL,
+	lastname varchar(128) NOT NULL,
+	department varchar(128),
+	phone varchar(50),
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE roles (
+	id int unsigned auto_increment NOT NULL,
+	role varchar(30) UNIQUE NOT NULL,
+	PRIMARY KEY(id)) engine=InnoDB;
+
+CREATE TABLE names (
+	id int unsigned auto_increment NOT NULL,
+	town_id int unsigned,
+	direction_id int unsigned,
+	name varchar(128) NOT NULL,
+	suffix_id int unsigned,
+	postDirection_id int unsigned,
+	startDate date,
+	endDate date,
+	notes varchar(255),
+	PRIMARY KEY(id),
+	FOREIGN KEY(town_id) REFERENCES towns (id),
+	FOREIGN KEY(suffix_id) REFERENCES suffixes (id),
+	FOREIGN KEY(direction_id) REFERENCES directions (id),
+	FOREIGN KEY(postDirection_id) REFERENCES directions (id)) engine=InnoDB;
+
+CREATE TABLE segments (
+	id int unsigned auto_increment NOT NULL,
+	tag varchar(8) NOT NULL,
+	startingNumber int unsigned NOT NULL,
+	endingNumber int unsigned NOT NULL,
+	jurisdiction_id int unsigned NOT NULL,
+	status_id int unsigned DEFAULT 1 NOT NULL,
+	notes varchar(255),
+	speedLimit tinyint(2) unsigned,
+	INDOTSegmentID varchar(10),
+	transportationClass varchar(5),
+	maintenanceResponsibility enum('UNKNOWN','STATE','DEVELOPER','BLOOMINGTON') DEFAULT 'UNKNOWN' NOT NULL,
+	leftLowNumber int(4) unsigned,
+	leftHighNumber int(4) unsigned,
+	rightLowNumber int(4) unsigned,
+	rightHighNumber int(4) unsigned,
+	rcinode1 char(8),
+	rcinode2 char(8),
+	lowNode char(8),
+	highNode char(8),
+	lowXCoordinate int(7) unsigned,
+	lowYCoordinate int(7) unsigned,
+	highXCoordinate int(7) unsigned,
+	highYCoordinate int(7) unsigned,
+	travel tinyint(1) unsigned,
+	travelDirection_id int unsigned,
+	complete char(2),
+	rclBack char(8),
+	rclNext char(8),
+	classRow int(4) unsigned,
+	mapArea char(3),
+	lastUpdatedDate date,
+	lastUpdatedAction varchar(30),
+	lastUpdatedUser_id int unsigned,
+	PRIMARY KEY(id),
+	FOREIGN KEY(jurisdiction_id) REFERENCES jurisdictions (id),
+	FOREIGN KEY(status_id) REFERENCES statuses (id),
+	FOREIGN KEY(travelDirection_id) REFERENCES directions (id),
+	FOREIGN KEY(lastUpdatedUser_id) REFERENCES users (id)) engine=InnoDB;
+
+CREATE TABLE places (
+	id int unsigned auto_increment NOT NULL,
+	name varchar(128),
+	township_id int unsigned,
+	jurisdiction_id int unsigned NOT NULL,
+	trashPickupDay_id int unsigned,
+	trashLargeItemPickupDay_id int unsigned,
+	recyclingPickupWeek_id int unsigned,
+	mailable tinyint(1) unsigned,
+	livable tinyint(1) unsigned,
+	section varchar(10),
+	quarterSection enum('NE','NW','SE','SW'),
+	class varchar(30),
+	PRIMARY KEY(id),
+	FOREIGN KEY(township_id) REFERENCES townships (id),
+	FOREIGN KEY(jurisdiction_id) REFERENCES jurisdictions (id),
+	FOREIGN KEY(trashPickupDay_id) REFERENCES trashPickupDays (id),
+	FOREIGN KEY(trashLargeItemPickupDay_id) REFERENCES trashPickupDays (id),
+	FOREIGN KEY(recyclingPickupWeek_id) REFERENCES recyclingPickupWeeks (id)) engine=InnoDB;
+
+CREATE TABLE buildings (
+	id int unsigned auto_increment NOT NULL,
+	place_id int unsigned NOT NULL,
+	name varchar(128),
+	startDate date NOT NULL,
+	endDate date,
+	GISTag varchar(20),
+	status_id int unsigned DEFAULT 1 NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(place_id) REFERENCES places (id),
+	FOREIGN KEY(status_id) REFERENCES statuses (id)) engine=InnoDB;
+
+CREATE TABLE units (
+	id int unsigned auto_increment NOT NULL,
+	place_id int unsigned NOT NULL,
+	unitType_id int unsigned NOT NULL,
+	building_id int unsigned,
+	identifier varchar(30),
+	mailable tinyint(1) unsigned,
+	livable tinyint(1) unsigned,
+	notes varchar(255),
+	PRIMARY KEY(id),
+	FOREIGN KEY(building_id) REFERENCES buildings (id),
+	FOREIGN KEY(unitType_id) REFERENCES unitTypes (id),
+	FOREIGN KEY(place_id) REFERENCES places (id)) engine=InnoDB;
+
+CREATE TABLE addresses (
+	id int unsigned auto_increment NOT NULL,
+	place_id int unsigned NOT NULL,
+	segment_id int unsigned  NOT NULL,
+	number int unsigned NOT NULL,
+	suffix varchar(30),
+	addressType varchar(20) DEFAULT 'STREET' NOT NULL,
+	city_id int unsigned NOT NULL,
+	zip int(5) unsigned zerofill,
+	zipplus4 int(4) unsigned zerofill,
+	status_id int unsigned DEFAULT 1 NOT NULL,
+	active enum('Y','N') DEFAULT 'Y' NOT NULL,
+	censusBlockFIPSCode varchar(20),
+	statePlaneX int unsigned,
+	statePlaneY int unsigned,
+	latitude float(10,6),
+	longitude float(10,6),
+	startDate date NOT NULL,
+	endDate date,
+	notes varchar(255),
+	PRIMARY KEY(id),
+	FOREIGN KEY(place_id) REFERENCES places (id),
+	FOREIGN KEY(segment_id) REFERENCES segments (id),
+	FOREIGN KEY(city_id) REFERENCES cities (id),
+	FOREIGN KEY(status_id) REFERENCES statuses (id)) engine=InnoDB;
+
+CREATE TABLE streets (
+	id int unsigned auto_increment NOT NULL,
+	notes varchar(255),
+	status_id int unsigned DEFAULT 1 NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(status_id) REFERENCES statuses (id)) engine=InnoDB;
+
+CREATE TABLE street_names (
+	street_id int unsigned NOT NULL,
+	name_id int unsigned NOT NULL,
+	streetNameType_id int unsigned,
+	primary key (street_id,name_id),
+	FOREIGN KEY(street_id) REFERENCES streets (id),
+	FOREIGN KEY(name_id) REFERENCES names (id),
+	FOREIGN KEY(streetNameType_id) REFERENCES streetNameTypes (id)) engine=InnoDB;
+
+CREATE TABLE route_streets (
+	route_id int unsigned NOT NULL,
+	street_id int unsigned NOT NULL,
+	primary key (route_id,street_id),
+	FOREIGN KEY(route_id) REFERENCES routes (id),
+	FOREIGN KEY(street_id) REFERENCES streets (id)) engine=InnoDB;
+
+CREATE TABLE plats (
+	id int unsigned auto_increment NOT NULL,
+	name varchar(128) NOT NULL,
+	township_id int unsigned,
+	platType_id int unsigned,
+	cabinet varchar(30),
+	envelope varchar(30),
+	notes varchar(255),
+	PRIMARY KEY(id),
+	FOREIGN KEY(township_id) REFERENCES townships (id),
+	FOREIGN KEY(platType_id) REFERENCES platTypes (id)) engine=InnoDB;
+
+CREATE TABLE subdivisionNames (
+	subdivision_id int unsigned auto_increment NOT NULL,
+	name varchar(128) NOT NULL,
+	startDate date NOT NULL,
+	endDate date,
+	status enum('Current','Renamed') DEFAULT 'Current' NOT NULL,
+	primary key (subdivision_id,name),
+	FOREIGN KEY(subdivision_id) REFERENCES subdivisions (id)) engine=InnoDB;
+
+CREATE TABLE subdivision_plats (
+	subdivision_id int unsigned NOT NULL,
+	plat_id int unsigned NOT NULL,
+	startDate date NOT NULL,
+	primary key (subdivision_id,plat_id),
+	FOREIGN KEY(plat_id) REFERENCES plats (id),
+	FOREIGN KEY(subdivision_id) REFERENCES subdivisions (id)) engine=InnoDB;
+
+CREATE TABLE street_segments (
+	street_id int unsigned NOT NULL,
+	segment_id int unsigned NOT NULL,
+	primary key (street_id,segment_id),
+	FOREIGN KEY(street_id) REFERENCES streets (id),
+	FOREIGN KEY(segment_id) REFERENCES segments (id)) engine=InnoDB;
+
+CREATE TABLE districts (
+	id int unsigned auto_increment NOT NULL,
+	name varchar(128) UNIQUE NOT NULL,
+	districtType_id int unsigned NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(districtType_id) REFERENCES districtTypes (id)) engine=InnoDB;
+
+CREATE TABLE district_places (
+	place_id int unsigned NOT NULL,
+	district_id int unsigned NOT NULL,
+	primary key (place_id,district_id),
+	FOREIGN KEY(district_id) REFERENCES places (id),
+	FOREIGN KEY(district_id) REFERENCES districts (id)) engine=InnoDB;
+
+CREATE TABLE place_status (
+	place_id int unsigned NOT NULL,
+	startDate date NOT NULL,
+	endDate date,
+	status_id int unsigned DEFAULT 1 NOT NULL,
+	primary key (place_id,startDate,status_id),
+	FOREIGN KEY(place_id) REFERENCES places (id),
+	FOREIGN KEY(status_id) REFERENCES statuses (id)) engine=InnoDB;
+
+CREATE TABLE placeHistory (
+	place_id int unsigned NOT NULL,
+	action varchar(30) NOT NULL,
+	date date NOT NULL,
+	notes varchar(255),
+	user_id int unsigned,
+	FOREIGN KEY(place_id) REFERENCES places (id),
+	FOREIGN KEY(user_id) REFERENCES users (id)) engine=InnoDB;
+
+CREATE TABLE user_roles (
+	user_id int unsigned NOT NULL,
+	role_id int unsigned NOT NULL,
+	primary key (user_id,role_id),
+	FOREIGN KEY(user_id) REFERENCES users (id),
+	FOREIGN KEY(role_id) REFERENCES roles (id)) engine=InnoDB;
