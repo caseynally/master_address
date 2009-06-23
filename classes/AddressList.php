960fc28c3a9f002c44750efe65e8aad9e5538c37
inghamn <inghamn@c0873d58-3416-0410-9558-ca141da00fa8>
2009-06-23 18:38:04 +0000

    Address searching is now working
    git-svn-id: svn+ssh://bandit.bloomington.in.gov/srv/svn/master_address/trunk@180 c0873d58-3416-0410-9558-ca141da00fa8

--- classes/AddressList.php
+++ classes/AddressList.php
@@ -19,7 +19,7 @@ class AddressList extends ZendDbResultIterator
 {
 	private $columns;
 	private static $directions = array();
-	private static $suffixes = array();
+	private static $streetTypes = array();
 	private static $subunitTypes = array();
 	private static $cities = array();
 
@@ -88,6 +88,10 @@ class AddressList extends ZendDbResultIterator
 		// If we pass in an address, we should parse the address string into the fields
 		if (isset($fields['address'])) {
 			$fields = self::parseAddress($fields['address']);
+			// We don't support searching for fractions right now
+			if (isset($fields['fraction'])) {
+				unset($fields['fraction']);
+			}
 			unset($fields['address']);
 		}
 
@@ -95,10 +99,56 @@ class AddressList extends ZendDbResultIterator
 		if (count($fields)) {
 			foreach ($fields as $key=>$value) {
 				if (array_key_exists($key,$this->columns)) {
-					$this->select->where("a.$key=?",$value);
+					$this->select->where("a.$key like ?","$value%");
 				}
 			}
 		}
+
+		// Finding on fields from other tables requires joining those tables.
+		// You can handle fields from other tables by adding the joins here
+		// If you add more joins you probably want to make sure that the
+		// above foreach only handles fields from the mast_address table.
+		$joins = array();
+		if (isset($fields['direction'])) {
+			$joins['s'] = array('table'=>'mast_street',
+								'condition'=>'a.street_id=s.street_id');
+			$this->select->where('s.street_direction_code=?',$fields['direction']->getCode());
+		}
+
+		if (isset($fields['postDirection'])) {
+			$joins['s'] = array('table'=>'mast_street',
+								'condition'=>'a.street_id=s.street_id');
+			$this->select->where('s.post_direction_suffix_code=?',
+								$fields['postDirection']->getCode());
+		}
+
+		if (isset($fields['street_name'])) {
+			$joins['s'] = array('table'=>'mast_street',
+								'condition'=>'a.street_id=s.street_id');
+			$joins['n'] = array('table'=>'mast_street_names',
+								'condition'=>'s.street_id=n.street_id');
+			$this->select->where('n.street_name like ?',"$fields[street_name]%");
+		}
+
+		if (isset($fields['streetType'])) {
+			$joins['s'] = array('table'=>'mast_street',
+								'condition'=>'a.street_id=s.street_id');
+			$joins['n'] = array('table'=>'mast_street_names',
+								'condition'=>'s.street_id=n.street_id');
+			$this->select->where('n.street_type_suffix_code=?',$fields['streetType']->getCode());
+		}
+
+		if (isset($fields['subunitType'])) {
+			$joins['u'] = array('table'=>'mast_address_subunits',
+								'condition'=>'a.street_address_id=u.street_address_id');
+			$this->select->where('u.sudtype=?',$fields['subunitType']->getType());
+		}
+
+		// Add all the joins we've created to the select
+		foreach ($joins as $key=>$join) {
+			$this->select->joinLeft(array($key=>$join['table']),$join['condition']);
+		}
+
 
 		$this->select->order($order);
 		if ($limit) {
@@ -107,7 +157,6 @@ class AddressList extends ZendDbResultIterator
 		if ($groupBy) {
 			$this->select->group($groupBy);
 		}
-		echo $this->select;
 		$this->populateList();
 	}
 
@@ -150,12 +199,13 @@ class AddressList extends ZendDbResultIterator
 	 */
 	public function parseAddress($string)
 	{
+		//echo "Starting with $string\n";
 		$address = preg_replace('/[^\w\s\/\-]/',' ',$string);
 		$address = preg_replace('/\s+/',' ',$address);
 
 		//echo "Looking for fractions: $address\n";
 		if (preg_match("/(?<fraction>\d+\/\d+)/",$address,$matches)) {
-			$output['fraction'] = $matches['fraction'];
+			$output['fraction'] = trim($matches['fraction']);
 			$address = preg_replace("/\d+\/\d+/",'',$address);
 		}
 
@@ -163,30 +213,30 @@ class AddressList extends ZendDbResultIterator
 		$directionCodePattern = implode('|',self::getDirections());
 		$numberPattern = "(?<number>\d+[\-\s]?(?:[^$directionCodePattern])?)";
 		if (preg_match("/^$numberPattern/i",$address,$matches)) {
-			$output['street_number'] = $matches['number'];
+			$output['street_number'] = trim($matches['number']);
 			$address = preg_replace("/^$matches[number]/i",'',$address);
 		}
 
 		//echo "Looking for Zip: $address\n";
 		$zipPattern = '(?<zip>\d{5})(\-(?<zipplus4>\d{4}))?';
 		if (preg_match("/\s$zipPattern\s?$/i",$address,$matches)) {
-			$output['zip'] = $matches['zip'];
+			$output['zip'] = trim($matches['zip']);
 			if (isset($matches['zipplus4']) && $matches['zipplus4']) {
-				$output['zipplus4'] = $matches['zipplus4'];
+				$output['zipplus4'] = trim($matches['zipplus4']);
 			}
 			$address = trim(preg_replace("/\s$zipPattern$/i",'',$address));
 		}
 
 		//echo "Looking for State: $address\n";
 		if (preg_match("/\s(?<state>IN|INDIANA)\b/i",$address,$matches)) {
-			$output['state'] = $matches['state'];
+			$output['state'] = trim($matches['state']);
 			$address = trim(preg_replace("/\s$matches[state]$/i",'',$address));
 		}
 
 		//echo "Looking for city: $address\n";
 		$cityPattern = implode('|',self::getCities());
 		if (preg_match("/\s(?<city>$cityPattern)$/i",$address,$matches)) {
-			$output['city'] = $matches['city'];
+			$output['city'] = trim($matches['city']);
 			$address = trim(preg_replace("/\s$matches[city]$/i",'',$address));
 		}
 
@@ -195,7 +245,13 @@ class AddressList extends ZendDbResultIterator
 														array_keys(self::getSubunitTypes())));
 		$subunitPattern = "(?<subunitType>$subunitTypePattern)(\-|\s)?(?<subunitIdentifier>\w+)";
 		if (preg_match("/\s(?<subunit>$subunitPattern)$/i",$address,$matches)) {
-			$output['subunitType'] = $matches['subunitType'];
+			try {
+				$output['subunitType'] = $matches['subunitType'];
+			}
+			catch (Exception $e) {
+				// Just ignore anything that's not a know type
+			}
+
 			$output['subunitIdentifier'] = $matches['subunitIdentifier'];
 			$address = trim(preg_replace("/\s$matches[subunit]$/i",'',$address));
 		}
@@ -203,8 +259,8 @@ class AddressList extends ZendDbResultIterator
 		//echo "Looking for Street Name: $address\n";
 		$fullDirectionPattern = implode('|',array_merge(self::getDirections(),
 														array_keys(self::getDirections())));
-		$streetTypePattern = implode('|',array_merge(self::getSuffixes(),
-													array_keys(self::getSuffixes())));
+		$streetTypePattern = implode('|',array_merge(self::getStreetTypes(),
+													array_keys(self::getStreetTypes())));
 		$streetPattern = "
 		(
 			(?<dir>$fullDirectionPattern)\s(?<type>$streetTypePattern)\b
@@ -234,25 +290,40 @@ class AddressList extends ZendDbResultIterator
 				switch ($key) {
 					case 'dir':
 					case 'direction':
-						$output['direction'] = $value;
+						try {
+							$output['direction'] = new Direction($value);
+						}
+						catch (Exception $e) {
+							// Just ignore anything that's not a known direction
+						}
 						break;
 
 					case 'type':
 					case 'streetType':
 					case 'stype':
-						$output['streetType'] = $value;
+						try {
+							$output['streetType'] = new StreetType($value);
+						}
+						catch (Exception $e) {
+							// Just ignore anything that's not a known street type
+						}
 						break;
 
 					case 'name':
 					case 'street':
 					case 'streetName':
-						$output['streetName'] = $value;
+						$output['street_name'] = $value;
 						break;
 
 					case 'postdirection':
 					case 'postdir':
 					case 'pdir':
-						$output['post_direction'] = $value;
+						try {
+							$output['postDirection'] = new Direction($value);
+						}
+						catch (Exception $e) {
+							// Just ignore anything that's not a known direction
+						}
 						break;
 				}
 			}
@@ -273,16 +344,16 @@ class AddressList extends ZendDbResultIterator
 		return self::$directions;
 	}
 
-	private static function getSuffixes()
+	private static function getStreetTypes()
 	{
-		if (!count(self::$suffixes)) {
-			$list = new SuffixList();
+		if (!count(self::$streetTypes)) {
+			$list = new StreetTypeList();
 			$list->find();
-			foreach ($list as $suffix) {
-				self::$suffixes[$suffix->getDescription()] = $suffix->getCode();
+			foreach ($list as $streetType) {
+				self::$streetTypes[$streetType->getDescription()] = $streetType->getCode();
 			}
 		}
-		return self::$suffixes;
+		return self::$streetTypes;
 	}
 
 	private static function getSubunitTypes()
