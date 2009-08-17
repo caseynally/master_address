a6ca775b9c75af665f820ced80b84b9ec49c2820
inghamn <inghamn@c0873d58-3416-0410-9558-ca141da00fa8>
2009-08-17 16:39:09 +0000

    Added Location service
    git-svn-id: svn+ssh://bandit.bloomington.in.gov/srv/svn/master_address/trunk@285 c0873d58-3416-0410-9558-ca141da00fa8

--- html/locations/home.php
+++ html/locations/home.php
@@ -4,21 +4,34 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
-$template = new Template();
+if (isset($_GET['format'])) {
+	switch ($_GET['format']) {
+		case 'xml':
+			$template = new Template('default','xml');
+			break;
+		default:
+			$template = new Template();
+	}
+}
+else {
+	$template = new Template();
+}
 
-$template->blocks[] = new Block('locations/findLocationForm.inc');
+if ($template->outputFormat == 'html') {
+	$template->blocks[] = new Block('locations/findLocationForm.inc');
+}
 if (isset($_GET['address'])) {
 	$locations = new LocationList();
 	$locations->find(array('address'=>$_GET['address']),'street_number');
 	$template->blocks[] = new Block('locations/locationList.inc',array('locationList'=>$locations));
 }
+if ($template->outputFormat == 'html') {
+	$purposeList = new PurposeList();
+	$purposeList->find();
+	$template->blocks[] = new Block('locations/purposeList.inc',array('purposeList'=>$purposeList));
 
-$purposeList = new PurposeList();
-$purposeList->find();
-$template->blocks[] = new Block('locations/purposeList.inc',array('purposeList'=>$purposeList));
-
-$types = new LocationTypeList();
-$types->find();
-$template->blocks[] = new Block('locations/locationTypeList.inc',array('locationTypeList'=>$types));
-
+	$types = new LocationTypeList();
+	$types->find();
+	$template->blocks[] = new Block('locations/locationTypeList.inc',array('locationTypeList'=>$types));
+}
 echo $template->render();
