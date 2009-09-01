4755948bc41eeb846849b1f4f733ba2aa825265a
inghamn <inghamn@c0873d58-3416-0410-9558-ca141da00fa8>
2009-09-01 20:18:17 +0000

    Created the start of new PHPUnit Testing
    git-svn-id: svn+ssh://bandit.bloomington.in.gov/srv/svn/master_address/trunk@312 c0873d58-3416-0410-9558-ca141da00fa8

---
+++ tests/DatabaseTests/LocationDbTest.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+
+class LocationDbTest extends PHPUnit_Framework_TestCase
+{
+	protected function setUp()
+	{
+	}
+	public function testSaveStatus()
+	{
+	}
+}
