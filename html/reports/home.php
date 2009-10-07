89f7f82982442188c8faa2997893893500e9799b
inghamn <inghamn@c0873d58-3416-0410-9558-ca141da00fa8>
2009-10-07 20:05:02 +0000

    Activity Reports are now available in TXT and XML formats
    git-svn-id: svn+ssh://bandit.bloomington.in.gov/srv/svn/master_address/trunk@434 c0873d58-3416-0410-9558-ca141da00fa8

---
+++ html/reports/home.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @copyright 2009 City of Bloomington, Indiana
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @author W Sibo <sibow@bloomington.in.gov>
+ */
+if (isset($_GET['actions'])) {
+	$actions = $_GET['actions'];
+
+	$types = array();
+	if (isset($_GET['types'])) {
+		$types = $_GET['types'];
+	}
+
+	$fields = array();
+	if (isset($_GET['dateFrom'])) {
+		if (is_array($_GET['dateFrom'])) {
+			if ($_GET['dateFrom']['mon']
+				&& $_GET['dateFrom']['mday'] && $_GET['dateFrom']['year']) {
+				$fields['dateFrom'] = new Date($_GET['dateFrom']);
+			}
+		}
+	}
+	if (isset($_GET['dateTo'])) {
+		if (is_array($_GET['dateTo'])) {
+			if ($_GET['dateTo']['mon'] && $_GET['dateTo']['mday']
+				&& $_GET['dateFrom']['year']) {
+				$fields['dateTo'] = new Date($_GET['dateTo']);
+			}
+		}
+	}
+
+	$changeLog = ChangeLog::getEntries($types,$actions,$fields);
+	$changeLogBlock = new Block('changeLogs/changeLog.inc',
+								array('changeLog'=>$changeLog,
+										'url'=>"http://$_SERVER[SERVER_NAME]$_SERVER[REQUEST_URI]"));
+}
+
+
+
+$template = isset($_GET['format'])
+			? new Template('default',$_GET['format'])
+			: new Template('two-column');
+if ($template->outputFormat == 'html') {
+	$template->blocks[] = new Block('reports/reportForm.inc');
+	if (isset($changeLogBlock)) {
+		$template->blocks['panel-one'][] = $changeLogBlock;
+	}
+}
+elseif (isset($changeLogBlock)) {
+	$template->blocks[] = $changeLogBlock;
+}
+
+echo $template->render();
