992f48f046ff6997a1eab8a114fb1081acacb024
inghamn <inghamn@c0873d58-3416-0410-9558-ca141da00fa8>
2009-10-12 17:38:41 +0000

    Moved the latest status change views to the Master Address schema
    git-svn-id: svn+ssh://bandit.bloomington.in.gov/srv/svn/master_address/trunk@445 c0873d58-3416-0410-9558-ca141da00fa8

--- scripts/migration/views.sql
+++ scripts/migration/views.sql
@@ -1,6 +1,6 @@
 -- You will need to run these as root.
 -- The ENG user does not have privileges to create views.
-create view eng.latest_subunit_status as
+create view master_address.latest_subunit_status as
 select z.*,s.status_code,l.description from eng.mast_address_subunit_status s
 left join eng.mast_address_status_lookup l on s.status_code=l.status_code
 right join (
@@ -8,7 +8,7 @@ right join (
 	from eng.mast_address_subunit_status group by subunit_id
 ) z on (s.subunit_id=z.subunit_id and s.start_date=z.start_date);
 
-create view eng.mast_address_latest_status as
+create view master_address.mast_address_latest_status as
 select z.*,s.status_code,l.description from eng.mast_address_status s
 left join eng.mast_address_status_lookup l on s.status_code=l.status_code
 right join (
