8704bb3b4fb8e856db0c3780d90c2a7261029c26
inghamn <inghamn@bloomington.in.gov>
2017-07-17 11:59:09 -0400

    Brought in generic Blossom scaffolding

--- build.sh
+++ build.sh
@@ -3,9 +3,25 @@ APPNAME=master_address
 DIR=`pwd`
 BUILD=$DIR/build
 
+declare -a dependencies=(msgfmt node-sass node npm)
+for i in "${dependencies[@]}"; do
+    command -v $i > /dev/null 2>&1 || { echo "$i not installed" >&2; exit 1; }
+done
+
+
 if [ ! -d $BUILD ]
 	then mkdir $BUILD
 fi
+
+# Compile the Lanague files
+cd $DIR/language
+./build_lang.sh
+cd $DIR
+
+# Compile the SASS
+cd $DIR/public/css
+./build_css.sh
+cd $DIR
 
 # The PHP code does not need to actually build anything.
 # Just copy all the files into the build
