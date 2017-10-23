158a6c467644b5c6e31952f5d430348479c4e0a8
inghamn <inghamn@bloomington.in.gov>
2017-10-23 15:42:13 -0400

    Updated street info view

---
+++ src/Views/Streets/InfoView.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright 2017 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ */
+declare (strict_types=1);
+namespace Application\Views\Streets;
+
+use Blossom\Classes\Block;
+use Blossom\Classes\Template;
+
+class InfoView extends Template
+{
+    public function __construct(array $vars)
+    {
+        $format = !empty($_REQUEST['format']) ? $_REQUEST['format'] : 'html';
+        parent::__construct('default', $format, $vars);
+
+
+        $this->blocks[]            = new Block('streets/info.inc',        ['street' => $this->street]);
+        $this->blocks['panel-one'] = new Block('streets/streetNames.inc', ['street' => $this->street]);
+    }
+}
